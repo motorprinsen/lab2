038c423c5d1d61fd4b87f7b1b2a98a2a6b757bae
Anders Ågren <anders.agren@rssoftware.se>
2023-03-14 23:03:08 +0100

    Wip

--- src/test/java/com/example/lab2/SvtPlayTests.java
+++ src/test/java/com/example/lab2/SvtPlayTests.java
@@ -1,11 +1,12 @@
 package com.example.lab2;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -14,7 +15,6 @@ import java.time.Duration;
 public class SvtPlayTests {
 
     private static WebDriver driver;
-    private static final String expectedTitle = "SVT Play";
 
     @BeforeAll
     static void setup() {
@@ -23,18 +23,24 @@ public class SvtPlayTests {
         driver = new ChromeDriver(options);
 
         driver.get("https://www.svtplay.se/");
-        getPassedTheCookieConsentModal();
+        acceptCookieConsentModal();
     }
 
-    static void getPassedTheCookieConsentModal() {
+    static void acceptCookieConsentModal() {
 
         var modalXpath = "//div[@data-rt='cookie-consent-modal']";
-        var wait = new WebDriverWait(driver, Duration.ofSeconds(5))
+
+        // The modal takes a couple of seconds to show up
+        new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(modalXpath)));
 
         var modal = driver.findElement(By.xpath(modalXpath));
         var consentButton = modal.findElement(By.xpath(".//button[text() = 'Acceptera alla']"));
         consentButton.click();
+
+        // The modal takes a couple of seconds to close after accepting
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(modalXpath)));
     }
 
     @BeforeEach
@@ -50,6 +56,7 @@ public class SvtPlayTests {
     @Test
     void isTitleCorrect() {
         var title = driver.getTitle();
+        var expectedTitle = "SVT Play";
 
         Assertions.assertEquals(expectedTitle, title);
     }
@@ -63,10 +70,121 @@ public class SvtPlayTests {
     }
 
     @Test
-    void checkWebsiteHeading() {
-        var xpath = "//*[@id=\"frontpage\"]/div/div[1]/div/div/div[1]/h1";
-        var h1 = driver.findElement(By.xpath(xpath));
-        Assertions.assertEquals("Här startar din IT-karriär!", h1.getText());
+    void areMainLinksOnStartPageCorrect() {
+        // Actual values are Pascal cased but then upper-cased using CSS
+        var expectedStartText = "START";
+        var startTextXpath = "//li[@type='start']/a";
+
+        var expectedProgramsText = "PROGRAM";
+        var expectedProgramsXpath = "//li[@type='programs']/a";
+
+        var expectedChannelsText = "KANALER";
+        var expectedChannelsXpath = "//li[@type='channels']/a";
+
+        var startText = driver.findElement(By.xpath(startTextXpath));
+        Assertions.assertEquals(expectedStartText, startText.getText());
+
+        var programsText = driver.findElement(By.xpath(expectedProgramsXpath));
+        Assertions.assertEquals(expectedProgramsText, programsText.getText());
+
+        var channelsText = driver.findElement(By.xpath(expectedChannelsXpath));
+        Assertions.assertEquals(expectedChannelsText, channelsText.getText());
+
     }
 
+    @Test
+    void checkAvailabilityLink() {
+        var expectedLinkText = "Tillgänglighet i SVT Play";
+        var linkXpath = "//a[@href='https://kontakt.svt.se/guide/tillganglighet']";
+
+        var link = driver.findElement(By.xpath(linkXpath));
+
+        Assertions.assertTrue(link.isDisplayed());
+
+        // Selenium cannot CSS select using compound class names,
+        // so we work around it by concatenating the names
+        var linkSpan = link.findElement(By.cssSelector(".sc-343fed33-3.dmRxHt"));
+        Assertions.assertEquals(expectedLinkText, linkSpan.getText());
+    }
+
+    @Test
+    void verifyHeadingInAvailabilitySite() {
+        var expectedText = "Så arbetar SVT med tillgänglighet";
+        var headingClass = "guide__title";
+        var linkXpath = "//a[@href='https://kontakt.svt.se/guide/tillganglighet']";
+
+        // Navigate to the availability site
+        driver.findElement(By.xpath(linkXpath)).click();
+
+        var heading = driver.findElement(By.className(headingClass));
+        Assertions.assertEquals(expectedText, heading.getText());
+    }
+
+    @Test
+    void checkNumberOfCategoriesOnProgramsPage() {
+        var programsXpath = "//li[@type='programs']/a";
+
+        // Navigate to the Programs page
+        driver.findElement(By.xpath(programsXpath)).click();
+
+        var expectedNoOfCategories = 18;
+        var categoriesXpath = "//article";
+
+        // NOTE: Had to add an implicit wait since the categories/articles seem to
+        //       take some time to load...
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        var categories = driver.findElements(By.xpath(categoriesXpath));
+
+        Assertions.assertEquals(expectedNoOfCategories, categories.size());
+    }
+
+    @Test
+    void verifyThatCookieSettingsAreRespected() {
+
+        // Grab the cookie consent cookie (Inception...)
+        var cookie = driver.manage().getCookieNamed("cookie-consent-1").toJson();
+
+        // We accepted all cookies in the setup
+        var expectedInitialAdStorageConsent = true;
+        var adStorageConsent = false;
+
+        try {
+            adStorageConsent = (boolean) new org.json.JSONObject(cookie.get("value").toString()).get("ad_storage");
+        } catch (JSONException e) {
+            // Invalid json in the cookie. We handle it in the assertion.
+        }
+
+        Assertions.assertEquals(expectedInitialAdStorageConsent, adStorageConsent);
+
+        // Navigate to the Setting page
+        var settingsLinkXpath = "//a[@class='sc-5b00349a-0 hwpvwu sc-87f10045-4 imzlFR' and @href='/installningar']";
+        driver.findElement(By.xpath(settingsLinkXpath)).click();
+
+        // Open the cookie consent dialog
+        var cookieButtonClass = ".sc-5b00349a-2.hLpVUw";
+        driver.findElement(By.cssSelector(cookieButtonClass)).click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("play_cookie_consent_ad_storage")));
+        // Toggle the ad storage consent switch
+        driver.findElement(By.name("play_cookie_consent_ad_storage")).click();
+
+        //var actions =  new Actions(driver);
+        //actions.moveToElement(driver.findElement(By.id("play_cookie_consent_ad_storage"))).click().perform();
+
+        // Grab the cookie again
+        driver.manage().getCookieNamed("cookie-consent-1").toJson();
+
+        var expectedAdStorageConsent = false;
+        var adStorageConsentAfterToggle = false;
+
+        try {
+            adStorageConsentAfterToggle = (boolean) new org.json.JSONObject(cookie.get("value").toString()).get("ad_storage");
+        } catch (JSONException e) {
+            // Invalid json in the cookie. We handle it in the assertion.
+        }
+
+        Assertions.assertEquals(expectedAdStorageConsent, adStorageConsentAfterToggle);
+    }
 }
