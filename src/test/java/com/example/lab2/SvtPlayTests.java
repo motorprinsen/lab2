567f548f2dba182c8959c5ebd7e02fac5f4c26d7
Anders Ågren <anders.agren@rssoftware.se>
2023-03-14 20:04:48 +0100

    Wip

--- src/test/java/com/example/lab2/SvtPlayTests.java
+++ src/test/java/com/example/lab2/SvtPlayTests.java
@@ -5,21 +5,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class SvtPlayTests {
 
     private static WebDriver driver;
+    private static final String expectedTitle = "SVT Play";
 
     @BeforeAll
     static void setup() {
         var options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--remote-allow-origins=*", "--incognito");
         driver = new ChromeDriver(options);
+
+        driver.get("https://www.svtplay.se/");
+        getPassedTheCookieConsentModal();
+    }
+
+    static void getPassedTheCookieConsentModal() {
+
+        var modalXpath = "//div[@data-rt='cookie-consent-modal']";
+        var wait = new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(modalXpath)));
+
+        var modal = driver.findElement(By.xpath(modalXpath));
+        var consentButton = modal.findElement(By.xpath(".//button[text() = 'Acceptera alla']"));
+        consentButton.click();
     }
 
     @BeforeEach
     void navigate() {
-        driver.get("https://iths.se");
+        driver.get("https://www.svtplay.se/");
     }
 
     @AfterAll
@@ -28,9 +48,18 @@ public class SvtPlayTests {
     }
 
     @Test
-    void checkWebsiteTitle() {
+    void isTitleCorrect() {
         var title = driver.getTitle();
-        Assertions.assertEquals("IT-Högskolan – Här startar din IT-karriär!", title);
+
+        Assertions.assertEquals(expectedTitle, title);
+    }
+
+    @Test
+    void isLogoVisible() {
+        var logoXpath = "//*[name()='svg']/*[name()='title' and text()='SVT Play logotyp']";
+        var logo = driver.findElement(By.xpath(logoXpath));
+
+        Assertions.assertTrue(logo.isDisplayed());
     }
 
     @Test
